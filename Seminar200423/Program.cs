c616c0df5b691443b512223370381f8a7f584d75
eldar <yaneproger@gmail.com>
2023-04-21 03:55:40 +0400

    Seminar 200423 updated with exercises

--- Seminar200423/Program.cs
+++ Seminar200423/Program.cs
@@ -110,3 +110,139 @@
 
 
 
+
+
+//Exercises for test checking after seminar at 210423
+
+
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+// Console.WriteLine("Enter first number");
+// int a = int.Parse(Console.ReadLine());
+// Console.WriteLine("Enter sec. number");
+// int b = int.Parse(Console.ReadLine());
+// int max = 0;
+
+// if (a > b)
+// {
+//     max = a;
+//     Console.WriteLine("max number is : " + max);
+// }
+// else
+//     Console.WriteLine("max number is : " + b);
+
+
+
+
+
+// Convert.ToInt32() can also cause this FormatException.
+
+// Check for int.Parse() and Convert.ToInt32() in your code.
+
+
+//     string input = Console.ReadLine();
+
+//     // ToInt32 can throw FormatException or OverflowException.
+//     try
+//     {
+//         numVal = Convert.ToInt32(input);
+//     }
+//     catch (FormatException e)
+//     {
+//         Console.WriteLine("Input string is not a sequence of digits.");
+//     }
+//     catch (OverflowException e)
+//     {
+//         Console.WriteLine("The number cannot fit in an Int32.");
+//     }
+
+// http://msdn.microsoft.com/en-us/library/bb397679.aspx
+
+
+
+
+
+
+
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Enter first number");
+// int a = int.Parse(Console.ReadLine());
+// Console.WriteLine("Enter sec. number");
+// int b = int.Parse(Console.ReadLine());
+// Console.WriteLine("Enter 3rd number");
+// int c = int.Parse(Console.ReadLine());
+// int max = 0;
+// if (a > b && a > c)
+// {
+//     max = a;
+//     Console.WriteLine("max number is : " + max);
+// }
+// else if (b > a && b > c)
+// {
+//     max = b;
+//     Console.WriteLine("max number is : " + max);
+// }
+// else
+//     max = c;
+// Console.WriteLine("max number is : " + max);
+
+
+
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+
+// Console.WriteLine("Enter a number");
+// int number = int.Parse(Console.ReadLine());
+
+// if (number == 0)
+// {
+//     Console.WriteLine("The number is a 0 ");
+// }
+
+// else if (number % 2 == 0)
+// {
+//     Console.WriteLine("The number is an even");
+// }
+// else
+//     Console.WriteLine("The number is an odd");
+
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+Console.WriteLine("Enter a number");
+int number = int.Parse(Console.ReadLine());
+int start = 1;
+while (start <= number)
+{
+    if (start % 2 == 0)
+    {
+        Console.Write(start + "  ");
+    }
+    start++;
+}
+
+
+
+
+
