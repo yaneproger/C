c1a1c7d4eb584797a7953523be91e601afc15bb0
eldar <yaneproger@gmail.com>
2023-04-22 03:28:42 +0400

    modiffied

--- ExampleUser/Program.cs
+++ ExampleUser/Program.cs
@@ -1,11 +1,19 @@
 ﻿
 
-Console.WriteLine("Enter your name");
+Console.Write("Enter your name  ");
 string username = Console.ReadLine();
 Console.WriteLine("Privet, " + username);
 Console.WriteLine(username);
 
 
+if (username.ToUpper() == "ELDAR")
+{
+    Console.WriteLine("Hello my dear friend!");
+}
+else
+{
+    Console.WriteLine("Hello " + username);
+}
 
 
 
