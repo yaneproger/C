d62be63b262e7ab82bd502ecab4d8130537aa00a
eldar <yaneproger@gmail.com>
2023-04-23 18:12:53 +0400

    points added

--- Seminar200423/Program.cs
+++ Seminar200423/Program.cs
@@ -270,5 +270,49 @@ if (e > max) max = e;
 Console.WriteLine("max number is  " + max);
 
 
+// Write multiple points
+Console.Clear();
+// Console.SetCursorPosition(10, 4);
+// Console.WriteLine("+");
+int xa = 1, ya = 30;
+int xb = 30, yb = 1;
+int xc = 60, yc = 30;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+int x = xa, y = xb;
+int count = 0;
+while (count < 10000)
+{
+    int point = new Random().Next(0, 3); //[0:3] 0 1 2
+    if (point == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if (point == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if (point == 2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
+
+
+
+
+
+
+
 
 
