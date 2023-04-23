ab57525bffe8a8d4377b73a2714032ab6bc3990f
eldar <yaneproger@gmail.com>
2023-04-23 04:35:41 +0400

    added -to find max from 5 numbers to Seminar200423

--- Seminar200423/Program.cs
+++ Seminar200423/Program.cs
@@ -254,6 +254,21 @@
 // Console.WriteLine(result);
 
 
+// to find max from 5 numbers 1 8 3 2 6
+
+// int a = 1;
+// int b = 8;
+// int c = 3;
+// int d = 2;
+// int e = 6;
+
+// int max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+// if (d > max) max = d;
+// if (e > max) max = e;
+// Console.WriteLine(max);
+
 
 
 
