cb09b8178a6a0d93a7012078cf23b63bb2f8ba89
yaneproger@gmail.com <yaneproger@gmail.com>
2023-07-12 20:39:27 -0400

    130723-04:39 commit

--- exercises110723/Program.cs
+++ exercises110723/Program.cs
@@ -1,2 +1,23 @@
 ﻿//new exercises130722
 //start new program
+int input(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine());
+    return result;
+}
+
+void NumberCheck(int arg1)
+{
+    if (arg1 %2==0)
+    {
+        Console.WriteLine("the number is even");
+    }
+    else 
+    {
+        Console.WriteLine("the number is odd");
+    }
+}
+
+int numcheck=input("enter a number");
+NumberCheck(numcheck);
