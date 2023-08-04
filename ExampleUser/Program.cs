d24fb61c06304a5e4a9002383dd7865be8618920
eldar <yaneproger@gmail.com>
2023-08-05 00:30:52 +0400

    050823-00:31-commit

--- ExampleUser/Program.cs
+++ ExampleUser/Program.cs
@@ -1,19 +1,19 @@
 ﻿
 
-Console.Write("Enter your name  ");
-string username = Console.ReadLine();
-// Console.WriteLine("Privet, " + username);
-Console.WriteLine(username);
+// Console.Write("Enter your name  ");
+// string username = Console.ReadLine();
+// // Console.WriteLine("Privet, " + username);
+// Console.WriteLine(username);
 
 
-if (username.ToUpper() == "ELDAR")
-{
-    Console.WriteLine("Hello my dear friend!");
-}
-else
-{
-    Console.WriteLine("Hello " + username);
-}
+// if (username.ToUpper() == "ELDAR")
+// {
+//     Console.WriteLine("Hello my dear friend!");
+// }
+// else
+// {
+//     Console.WriteLine("Hello " + username);
+// }
 
 
 
