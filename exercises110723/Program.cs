10fc13e7e8f0a1424f24c9195f34bb645c70a684
eldar <yaneproger@gmail.com>
2023-08-06 01:40:08 +0400

    060823-commit-01:40

--- exercises110723/Program.cs
+++ exercises110723/Program.cs
@@ -1896,60 +1896,51 @@
 
 // [3 7 22 2 78] -> 76
 
+void FillArray(int[] arr, int arg2)
+{
+    for (int index = 0; index < arr.Length; index++)
+    {
+        arr[index] = new Random().Next(0, 99);
+    }
+}
 
-// void FillArray(int[] arr, int arg2)
-// {
-//     for (int index = 0; index < arr.Length; index++)
-//     {
-//         arr[index] = new Random().Next(0, 99);
-//     }
-// }
+void PrintArray(int[] arr)
+{
+    for (int index = 0; index < arr.Length; index++)
+    {
+        Console.Write($" {arr[index]} , ");
+    }
+}
 
-// void PrintArray(int[] arr)
-// {
-//     for (int index = 0; index < arr.Length; index++)
-//     {
-//         Console.Write($" {arr[index]} , ");
-//         // Console.WriteLine();
+void MaxMinDiffer(int[] arr)
+{
+    int max = arr[0];
+    int min = arr[0];
 
-//     }
+    for (int index = 0; index < arr.Length; index++)
+    {
+        if (arr[index] > max)
+            max = arr[index];
+        if (arr[index] < min)
+            min = arr[index];
+    }
 
-// }
+    int differ = max - min;
+    Console.WriteLine($"  max is {max} ");
+    Console.WriteLine($"  min is {min} ");
 
+    Console.WriteLine($"  Diffenrence is {differ} ");
 
-// void MaxMinDiffer(int[] arr)
-// {
-//     int max = arr[0];
-//     int min = arr[0];
+}
 
-//     for (int index = 0; index < arr.Length; index++)
-//     {
-//         // max = arr[0];
-//         // min = arr[0];
+Console.WriteLine("enter array's length");
+int arrlength = int.Parse(Console.ReadLine());
+int[] array = new int[arrlength];
 
-//         if (arr[index] > max)
-//             max = arr[index];
-//         if (arr[index] < min)
-//             min = arr[index];
-//     }
-
-//     int differ = max - min;
-//     Console.WriteLine($"  max is {max} ");
-//     Console.WriteLine($"  min is {min} ");
-
-//     Console.WriteLine($"  Diffenrence is {differ} ");
-
-// }
-
-
-// Console.WriteLine("enter array's length");
-// int arrlength = int.Parse(Console.ReadLine());
-// int[] array = new int[arrlength];
-
-// FillArray(array, arrlength);
-// PrintArray(array);
-// Console.WriteLine();
-// MaxMinDiffer(array);
+FillArray(array, arrlength);
+PrintArray(array);
+Console.WriteLine();
+MaxMinDiffer(array);
 
 
 
