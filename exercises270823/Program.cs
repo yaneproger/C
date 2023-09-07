cf7b17437774db4eadcb5defe28a28160871fca3
yaneproger@gmail.com <user@localhost.localdomain>
2023-09-06 21:45:25 -0400

    070923-05;45-commit

--- exercises270823/Program.cs
+++ exercises270823/Program.cs
@@ -2458,6 +2458,7 @@ void Convertarray(int[] array)
 
 
 
+
 // Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
 // Если N = 5 -> 0 1 1 2 3
 // Если N = 3 -> 0 1 1
@@ -2504,6 +2505,7 @@ void Convertarray(int[] array)
 // PrintArray(newarr);
 // Console.WriteLine();
 // Copyarray(newarr);
+
 
 
 
