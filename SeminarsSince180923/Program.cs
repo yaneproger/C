84e25925a9a71701c094b493f87d63a0cab3b496
yaneproger@gmail.com <yaneproger@gmail.com>
2023-11-24 17:52:27 -0500

    new folder added-251123-02:53

--- SeminarsSince180923/Program.cs
+++ SeminarsSince180923/Program.cs
@@ -3835,36 +3835,246 @@
 // Заполните спирально массив 4 на 4.
 // Домашнее задание
 
+// Console.WriteLine("Enter layers number");
+// int layer = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Enter rows number");
+// int rows = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Enter colums number");
+// int columns = int.Parse(Console.ReadLine()!);
 
-int[,] array = new int[4, 4];
+// int[,,] FillArray(int layer, int rows, int columns)
+// {
+//     int[,,] array = new int[layer, rows, columns];
+//     for (int i = 0; i < layer; i++)
+//     {
+//         // Console.WriteLine($"Номер строки {i}");
+
+//         for (int j = 0; j < columns; j++)
+//         {
+//             // array[i, j] = new Random().Next(10, 100);
+//             for (int k = 0; k < rows; k++)
+//             {
+//                 // array[j, i] = new Random().Next(10, 100);
+
+//                 array[j, i, k] = int.Parse(Console.ReadLine()!);
+
+//                 Console.Write($"элемент- {array[j, i, k]}   -  {i},{j},{k}");
+//             }
+//             Console.WriteLine();
+//         }
+//         // Console.Write($"элемент- {array[i, j, k]}   -  {i},{j},{k}");
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[,,] array)
+// {
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//         {
+//             for (int k = 0; k < rows; k++)
+//             {
+//                 Console.Write($"элемент-{array[i, j, k]}- {i},{j},{k}  ");
+//             }
+//             Console.WriteLine($"Номер строки {j}");
+//         }
+//     }
+// }
+
+// int[,,] newarray = FillArray(layer, rows, columns);
+// PrintArray(newarray);
+
+
+
+
+// Console.WriteLine("Enter rows number");
+// int rows = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Enter colums number");
+// int columns = int.Parse(Console.ReadLine()!);
+
+// int[,] FillArray(int rows, int columns)
+// {
+//     int[,] array = new int[rows, columns];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         // Console.WriteLine($"Номер строки {i}");
+
+//         for (int j = 0; j < columns; j++)
+//         {
+//             // array[i, j] = new Random().Next(10, 100);
+//             Console.WriteLine($"Enter элемент {i},{j}");
+//             array[i, j] = int.Parse(Console.ReadLine()!);
+
+//             Console.Write($"элемент-{i},{j}-{array[i, j]}");
+//             Console.WriteLine();
+//         }
+//         Console.WriteLine();
+//         // Console.Write($"элемент- {array[i, j, k]}   -  {i},{j},{k}");
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[,] array)
+// {
+//     for (int i = 0; i < rows; i++)
+//     {
+//         Console.WriteLine($" Номер строки {i}");
+//         for (int j = 0; j < columns; j++)
+//         {
+//             Console.Write($" элемент-{i},{j}- {array[i, j]}");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// int[,] newarray = FillArray(rows, columns);
+// PrintArray(newarray);
+
+
+
+
+
+// Итоговая контрольная работа по основному блоку
+// Урок 1. Контрольная работа
+// Данная работа необходима для проверки ваших знаний и навыков 
+// по итогу прохождения первого блока обучения на программе Разработчик. 
+// Мы должны убедится, что базовое знакомство с IT прошло успешно.
+
+
+// Задача алгоритмически не самая сложная, 
+// однако для полноценного выполнения проверочной работы необходимо:
+
+
+// 1. Создать репозиторий на GitHub
+// 2. Нарисовать блок-схему алгоритма 
+// (можно обойтись блок-схемой основной содержательной части, 
+// если вы выделяете её в отдельный метод)
+// 3. Снабдить репозиторий оформленным текстовым описанием решения 
+// (файл README.md)
+// 4. Написать программу, решающую поставленную задачу
+// 5. Использовать контроль версий в работе над этим небольшим проектом 
+// (не должно быть так, что всё залито одним коммитом, 
+// как минимум этапы 2, 3, и 4 должны быть расположены в разных коммитах)
+
+
+
+// Задача: Написать программу, 
+// которая из имеющегося массива строк формирует новый массив из строк, 
+
+// длина которых меньше, либо равна 3 символам. 
+
+// Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+
+
+
+// using System;
+
+// public class Answer
+// {
 
 Console.WriteLine("Enter rows number");
-int rows = int.Parse(Console.ReadLine()!);
+int rows = int.Parse(Console.ReadLine());
 Console.WriteLine("Enter colums number");
-int columns = int.Parse(Console.ReadLine()!);
+int columns = int.Parse(Console.ReadLine());
 
 
-int[,] FillArray(int rows, int columns)
+string[,] FillArray(int rows, int columns)
 {
+    string[,] array = new string[rows, columns];
     for (int i = 0; i < rows; i++)
     {
-        Console.WriteLine($"Номер строки {i}");
-
+        Console.WriteLine("1st row");
         for (int j = 0; j < columns; j++)
         {
-            array[i, j] = new Random().Next(10, 100);
-
-            Console.Write($"элемент- {array[i, j]}   -  {i},{j},");
+            Console.WriteLine("Enter massive's element");
+            array[i, j] = Console.ReadLine()!;
         }
         Console.WriteLine();
+        Console.WriteLine("next row");
     }
     return array;
 }
 
-int[,] newarray = FillArray(rows, columns);
+
+void PrintArray(string[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine($" Номер строки {i}");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($" элемент-{i},{j}- {array[i, j]}");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int ElementCount(string[,] array)
+
+{
+    int arraysize = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j].Length >= 3)
+            {
+                arraysize = arraysize + 1;
+            }
+        }
+    }
+    Console.WriteLine($"arraysize{arraysize}");
+    return arraysize;
+}
+
+
+
+
+string[] ChangedArray(string[,] array, int arraysize)
+
+{
+    string[] newarray = new string[arraysize];
+
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j].Length >= 3)
+            {
+                newarray[i] = array[i, j];
+                Console.Write($"newarray [{newarray[i]} ]");
+            }
+        }
+        // Console.WriteLine(string.Join(", ", array.Where(x => x.Length > 3)));
+    }
+    return newarray;
+}
 
 
 
 
 
 
+
+
+Console.Clear();
+string[,] newarray = FillArray(rows, columns);
+PrintArray(newarray);
+int newarraysize = ElementCount(newarray);
+Console.WriteLine("newarray");
+ChangedArray(newarray, newarraysize);
+// Console.WriteLine("newarray");
+// PrintArray(ChangeArray(newarray));
